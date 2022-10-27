52d45540e1884e90d5a7547ffbb31f03addbccb2
Meier <valerimeier23@gmail.com>
2022-10-27 16:18:04 +0200

    Updates

---
+++ precache-manifest.8786bd4ba78292723ff5acfdc4e17130.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da6302d8a28571bf06539f05d39f8ba",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "2137c4f08920471290e3",
+    "url": "/react-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "b9ba74a1c25b61b32810",
+    "url": "/react-invoice-generator/static/css/main.96610729.chunk.css"
+  },
+  {
+    "revision": "2137c4f08920471290e3",
+    "url": "/react-invoice-generator/static/js/2.e222a0c1.chunk.js"
+  },
+  {
+    "revision": "5fae47d0a016a5b68e2af829b08bec2a",
+    "url": "/react-invoice-generator/static/js/2.e222a0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ba74a1c25b61b32810",
+    "url": "/react-invoice-generator/static/js/main.1b86c69c.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "e631f0ca65031b20fe23668e166aa85b",
+    "url": "/react-invoice-generator/static/media/remove.e631f0ca.svg"
+  },
+  {
+    "revision": "a700735566a3701072afbd164b80e690",
+    "url": "/react-invoice-generator/static/media/resize.a7007355.svg"
+  }
+]);
