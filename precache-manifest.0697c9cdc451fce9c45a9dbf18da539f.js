06bda7b3b357870013ecb8d119fbbceb457b4efd
Valtrahi2311 <valtrahi2311@users.noreply.github.com>
2022-10-27 16:18:04 +0200

    feat: Deploy React Invoice Generator to GitHub Pages
    - Complete invoice generator with PDF export functionality
    - German localization support
    - SEPA QR code generation
    - Responsive design with modern UI
    - Logo upload and customization features

---
+++ precache-manifest.0697c9cdc451fce9c45a9dbf18da539f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0836febaf0dc0521ce695c5d38c660ca",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "d9bf15c9323ddbeeb818",
+    "url": "/react-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "d8a6bccb579412b2849b",
+    "url": "/react-invoice-generator/static/css/main.082ba511.chunk.css"
+  },
+  {
+    "revision": "d9bf15c9323ddbeeb818",
+    "url": "/react-invoice-generator/static/js/2.a537da24.chunk.js"
+  },
+  {
+    "revision": "06287f0268467607830b0ffa0b069a13",
+    "url": "/react-invoice-generator/static/js/2.a537da24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a6bccb579412b2849b",
+    "url": "/react-invoice-generator/static/js/main.56c186b3.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "3d141c5ada84599650af16e5f1534aad",
+    "url": "/react-invoice-generator/static/media/download-white.3d141c5a.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/react-invoice-generator/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/react-invoice-generator/static/media/resize.d4da359f.svg"
+  }
+]);
